e7d8d49f78df49d45220271497d59662c6b5bb36
Alena Huang <alenahuang1@gmail.com>
2018-02-16 12:06:13 -0600

    Test Class Versions

--- src/TestClass1.java
+++ src/TestClass1.java
@@ -1,9 +1,31 @@
-
+import java.util.Scanner;
 public class TestClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		Greeter alena = new Greeter();
+		
+		alena.toString();
+		
+		Greeter[] array = new Greeter[5];
+		
+		for (int i = 0; i > array.length; i = i +1) {
+			System.out.println("Name");
+			String xname = input.nextLine();
+			System.out.println("age");
+			int xage = input.nextInt();
+			
+			array[i] = new Greeter(xname, xage);
+		}
+		
+		for (int i = 0; i > array.length; i = i +1) {
+			System.out.println(array[i].toString());
+		}
+		
+		
+		
 	}
 
 }
